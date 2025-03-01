b876acd9e8e215bd710a5ca3b7e0f693aaabe86c
garg1976 <garg1976kk@gmail.com>
2025-03-01 09:47:01 +0530

    day1

--- src/Component/Child3.js
+++ src/Component/Child3.js
@@ -1,21 +1,35 @@
 import React from 'react'
 import { useContext } from 'react';
+
 // import { FirstContext } from '../../Context/FirstContext';
-const Child3 = ({getData}) => {
+// const Child3 = ({getData}) => {
     // const  {firstName, LastName, otherData, combineData}
-   const data = useContext(useContext)
+  //  const data = useContext(useContext)
     //  //accessing data from the context
-    const styles = {
-        border: "1px solid black",
-        margin: "50px 50px 50px 50px",
-        padding: "50px 50px 50px 50px",
-      };
-      console.log(data, "data")
+//     const styles = {
+//         border: "1px solid black",
+//         margin: "50px 50px 50px 50px",
+//         padding: "50px 50px 50px 50px",
+//       };
+//       console.log(data, "data")
+//   return (
+//     <div style={styles}>I am Child3
+//         <button onClick={getData}>Get Data from Parent</button>
+//     </div>
+//   )
+// }
+
+const styles = {
+  border: "1px solid black",
+  margin: "50px 50px 50px 50px",
+  padding: "50px 50px 50px 50px",
+};
+const Child3 = ({ data }) => {
   return (
-    <div style={styles}>I am Child3
-        <button onClick={getData}>Get Data from Parent</button>
-    </div>
-  )
-}
+    <button onClick={() => data.combineData("Karan", "Garg")}>
+      Send data to parent
+    </button>
+  );
+};
 
 export default Child3
